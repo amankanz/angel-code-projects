9c54df47b155cfaaffbce0efbbc2ce025926a707
amanikaneza <akanezamulume.iuea@gmail.com>
2023-03-28 08:48:57 +0300

    html, css & js

--- ziha-web-3.0/script.js
+++ ziha-web-3.0/script.js
@@ -5,3 +5,13 @@ menuBtn.addEventListener("click", function (e) {
   document.querySelector("body").classList.toggle("mobile_nav_active");
   this.classList.toggle("fa-xmark");
 });
+
+//Typing effect
+
+let typed = new Typed(".auto_input", {
+  strings: ["Software Engineer!", "Web Developer!", "Front-End Engineer!"],
+  typedSpeed: 100,
+  backSpeed: 100,
+  backDelay: 2000,
+  loop: true,
+});
